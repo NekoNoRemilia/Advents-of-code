10226c1ef373c6a2f24d3dfe10bcf1a6584d0915
Gabi <112790911+NekoNoRemilia@users.noreply.github.com>
2023-01-03 09:16:01 -0300

    Add files via upload

---
+++ elves-calories.php
@@ -0,0 +1,68 @@
+<?php
+/* Day 1: Calorie Counting (https://adventofcode.com/2022/day/1)
+# Conditions:
+# - The input file represents inventories
+# - Inventory consists of calories seperated by new line
+# - Each inventory is seperated by empty line */ 
+
+function dd($data){
+    echo "<pre>";
+    var_dump($data);
+    echo "</pre>";
+    // die();
+}; ?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title> Advent of Code day 01 2022 (?)</title>
+    </head>
+    <body>
+
+
+        <h1><?php echo "Advent of code, day 01."; ?></h1>
+
+
+        <?php
+            $myInput = fopen("input", "r") or die("Unable to open file!");
+        function task1($file, $file_name){
+            $inventory = 0;
+            $biggest_inventory = 0;
+            foreach(preg_split("/((\r?\n)|(\r\n?))/", fread($file, filesize($file_name))) as $line){
+                if(PHP_EOL){
+                    // echo 'if working';
+                    if ($inventory < $biggest_inventory){
+                        $inventory = $biggest_inventory;
+                    }
+                    $inventory = 0;
+                } else {
+                    // echo 'now my else works';
+                    $inventory = $inventory + $line;
+                }
+            } 
+        
+            return $biggest_inventory;
+        }
+        function task2($file, $file_name){
+            $inventory = 0;
+            $inventory_list = [];
+            foreach (preg_split("/((\r?\n)|(\r\n?))/", fread($file, filesize($file_name))) as $line) {
+                if(PHP_EOL){
+                    array_push($inventory_list, $inventory);
+                    $inventory = 0;
+                }
+                else {
+                    $inventory += $line;
+                }
+                return $sum_top_inventories = array_sum($inventory_list);
+            }   
+        }
+        ?> 
+        <?php 
+        // dd(task1($myInput, "input"));
+        // task1($myInput, "input");
+        // task2($myInput, "input"); ?>
+        <h1>Task number 1 result: <?php echo task1($myInput, "input") ; ?></h1>
+        <h1>Task number 2 result: <?php echo task2($myInput, "input") ; ?></h1>
+      
+    </body>
+</html>
